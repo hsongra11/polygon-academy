76043a513fe174167244ed71aa03fcdc80ff9421
hsongra <harsh.songra@qyuki.com>
2022-02-11 12:16:49 +0530

    adding folder for module1

---
+++ module1/src/App.js
@@ -0,0 +1,35 @@
+import {Web3ReactProvider} from '@web3-react/core';
+import {Web3Provider} from '@ethersproject/providers';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Content from './components/Containers';
+import {Container, Image} from 'semantic-ui-react';
+import iconDevx from "./assets/icon-devx.svg"
+
+import './App.css';
+
+const getLibrary = (provider) => {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 8000;
+  return library;
+};
+
+function App() {
+  return (
+  <Web3ReactProvider getLibrary={getLibrary}>
+    <div className="App">
+      <Header/>
+      <Content>
+        <Container text textAlign='center'>
+          <p>Hello World</p>
+        </Container>
+      </Content>
+      <Footer/>
+    </div>
+  </Web3ReactProvider>
+  );
+}
+
+export default App;
